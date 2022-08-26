a7bf2e11366166549fa6d0f4fa2426d130c96acd
ernesto baquero <ernest.baquero@gmail.com>
2022-08-26 12:47:50 +0100

    updating views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,15 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :poster_url, :rating, :trailer)
+  end
+
 end
